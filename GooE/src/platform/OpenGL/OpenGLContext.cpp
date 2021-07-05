3ab1eea0f400e1d4a3b084d066635160b7c7f6b7
Stéphane Loegel <loegel.stephane@gmail.com>
2021-07-05 23:19:24 +0200

    Log information about GPU and Renderer

--- GooE/src/platform/OpenGL/OpenGLContext.cpp
+++ GooE/src/platform/OpenGL/OpenGLContext.cpp
@@ -12,6 +12,11 @@ namespace GooE {
 		glfwMakeContextCurrent(window);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		GOOE_CORE_ASSERT(status, "Failed to initialize Glad!")
+
+		GOOE_CORE_INFO("OpenGL Renderer:");
+		GOOE_CORE_INFO("  Vendor:   {0}", glGetString(GL_VENDOR));
+		GOOE_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		GOOE_CORE_INFO("  Version:  {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers() {
