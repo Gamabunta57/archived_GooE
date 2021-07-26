7cad347fca918278daee5570e25c146354eb87bb
Stéphane Loegel <loegel.stephane@gmail.com>
2021-07-27 00:20:06 +0200

    Apply instrumentor fix from Hazel #185

--- GooE/src/GooE/Debug/Instrumentor.h
+++ GooE/src/GooE/Debug/Instrumentor.h
@@ -18,14 +18,19 @@
 #include <chrono>
 #include <algorithm>
 #include <fstream>
-
+#include <iomanip>
 #include <thread>
 
 namespace GooE {
+
+    using FloatMs = std::chrono::duration<double, std::micro>;
+
     struct ProfileResult
     {
         std::string Name;
-        long long Start, End;
+
+        FloatMs Start;
+        std::chrono::microseconds ElapsedTime;
         uint32_t ThreadID;
     };
 
@@ -37,52 +42,74 @@ namespace GooE {
     class Instrumentor
     {
     private:
+        std::mutex m_Mutex;
         InstrumentationSession* m_CurrentSession;
         std::ofstream m_OutputStream;
-        int m_ProfileCount;
     public:
         Instrumentor()
-            : m_CurrentSession(nullptr), m_ProfileCount(0)
+            : m_CurrentSession(nullptr)
         {
         }
 
         void BeginSession(const std::string& name, const std::string& filepath = "results.json")
         {
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentSession) {
+                // If there is already a current session, then close it before beginning new one.
+                // Subsequent profiling output meant for the original session will end up in the
+                // newly opened session instead.  That's better than having badly formatted
+                // profiling output.
+                GOOE_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+                InternalEndSession();
+            }
             m_OutputStream.open(filepath);
-            WriteHeader();
-            m_CurrentSession = new InstrumentationSession{ name };
+
+            if (m_OutputStream.is_open()) {
+                m_CurrentSession = new InstrumentationSession({ name });
+                WriteHeader();
+            } else {
+                GOOE_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+            }
         }
 
         void EndSession()
         {
-            WriteFooter();
-            m_OutputStream.close();
-            delete m_CurrentSession;
-            m_CurrentSession = nullptr;
-            m_ProfileCount = 0;
+            std::lock_guard lock(m_Mutex);
+            InternalEndSession();
         }
 
         void WriteProfile(const ProfileResult& result)
         {
-            if (m_ProfileCount++ > 0)
-                m_OutputStream << ",";
+            std::stringstream json;
 
             std::string name = result.Name;
             std::replace(name.begin(), name.end(), '"', '\'');
 
-            m_OutputStream << "{";
-            m_OutputStream << "\"cat\":\"function\",";
-            m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-            m_OutputStream << "\"name\":\"" << name << "\",";
-            m_OutputStream << "\"ph\":\"X\",";
-            m_OutputStream << "\"pid\":0,";
-            m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-            m_OutputStream << "\"ts\":" << result.Start;
-            m_OutputStream << "}";
+            json << std::setprecision(3) << std::fixed;
+            json << "{";
+            json << "\"cat\":\"function\",";
+            json << "\"dur\":" << result.ElapsedTime.count() << ',';
+            json << "\"name\":\"" << name << "\",";
+            json << "\"ph\":\"X\",";
+            json << "\"pid\":0,";
+            json << "\"tid\":" << result.ThreadID << ",";
+            json << "\"ts\":" << result.Start.count();
+            json << "},";
 
-            m_OutputStream.flush();
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentSession) {
+                m_OutputStream << json.str();
+                m_OutputStream.flush();
+            }
         }
 
+        static Instrumentor& Get()
+        {
+            static Instrumentor instance;
+            return instance;
+        }
+
+    private:
         void WriteHeader()
         {
             m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
@@ -91,14 +118,19 @@ namespace GooE {
 
         void WriteFooter()
         {
-            m_OutputStream << "]}";
+            m_OutputStream
+                << "{}" //add empty event to avoid JSON syntax error
+                << "]}";
             m_OutputStream.flush();
         }
 
-        static Instrumentor& Get()
-        {
-            static Instrumentor instance;
-            return instance;
+        void InternalEndSession() {
+            if (m_CurrentSession) {
+                WriteFooter();
+                m_OutputStream.close();
+                delete m_CurrentSession;
+                m_CurrentSession = nullptr;
+            }
         }
     };
 
@@ -120,9 +152,10 @@ namespace GooE {
         void Stop()
         {
             auto endTimepoint = std::chrono::high_resolution_clock::now();
-
-            long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-            long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+            FloatMs start = FloatMs{ m_StartTimepoint.time_since_epoch() };
+            auto end =
+                std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch()
+                - std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch();
 
             uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
             Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
